346d0e8b96075497346d328d92c6563689cfa2fe
JamesOdera <nyangoma.odera@gmail.com>
2020-03-01 12:45:28 +0300

    Add date fxn for the days an album is ncreated

--- album/views.py
+++ album/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return HttpResponse('IMAGE GALLERY')
+
+def album_of_day(request):
+    date = dt.date.today()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Images for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    
+    day = days[day_number]
+    return day
+
+def past_days_album(request,past_date):
+        
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Image for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
